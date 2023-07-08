002a50b3a51c3867c5b59f7b328ba4dca7304c55
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-08 13:24:15 +0700

    create show table

--- src/main/java/com/mycompany/xojgob/XoJGob.java
+++ src/main/java/com/mycompany/xojgob/XoJGob.java
@@ -4,13 +4,35 @@
 
 package com.mycompany.xojgob;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USER
  */
 public class XoJGob {
+     public String[][] list = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
+    public String start;
+    public String turn;
+    public boolean confirm;
+    public int row;
+    public int column;
+    public boolean isEnd = false;
 
+    Scanner sc = new Scanner(System.in);
+ public void showTable() {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                System.out.print(" " + list[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        XoJGob xo = new XoJGob();
+        xo.showTable();
     }
 }
