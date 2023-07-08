eeeef6c4b6c1628ebf81b545047cfcc7591d5e68
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-08 13:20:47 +0700

    create project

---
+++ src/main/java/com/mycompany/xojgob/XoJGob.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xojgob;
+
+/**
+ *
+ * @author USER
+ */
+public class XoJGob {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
